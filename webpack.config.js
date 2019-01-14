2e6f2a241276dedf854f99aa5c55c00584142817
MeirOverferst <meiroverferst@gmail.com>
2019-01-14 10:23:39 +0100

    set up the rules for webpack -final stage

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ module.exports = {
     mode: 'development',
     entry: './src/scripts/app.js',
     output: { path: path.resolve(__dirname, 'dist'), filename: 'bundle.js' },
-    module: { rules: [{}] },
+    module: {
+        rules: [{
+            test: /\.js?/i, exclude: /node_modules/i, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-env'] } }
+        }]
+    },
     plugins: []
 }
